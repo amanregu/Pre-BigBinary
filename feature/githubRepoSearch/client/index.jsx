cb429331a8a5e1abbed6d908caa0eb0af3d0808d
aman <amanregu@gmail.com>
2020-02-21 01:30:20 +0530

    Add App component

--- feature/githubRepoSearch/client/index.jsx
+++ feature/githubRepoSearch/client/index.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
-);
+import DisplayRepositories from './components/DisplayRepositories';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFetching: false,
+      repositories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchRepositories();
+  }
+
+  async fetchRepositories() {
+    this.setState({
+      isFetching: true,
+    });
+    const responce = await axios.get('http://localhost:3000/repositories/get');
+    this.setState({
+      repositories: responce.data.repositories,
+      isFetching: false,
+    });
+  }
+
+  render() {
+    const { repositories, isFetching } = this.state;
+    return (
+      <div>
+        {isFetching ? (
+          <div>
+            <h1>
+              LOADING.......
+            </h1>
+          </div>
+        ) : (
+          <div>
+            <DisplayRepositories repositories={repositories} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
