116c8c5b3f5eeb7e3e5e522bb7e8b042625d6afb
aman <amanregu@gmail.com>
2020-02-21 17:57:54 +0530

    fix: indentation

--- feature/githubRepoSearch/models/repository.js
+++ feature/githubRepoSearch/models/repository.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+
 const { Schema } = mongoose;
 
 const typeEnum = ['Public', 'Private', 'Sources', 'Forks', 'Archived', 'Mirrors'];
 
 const repositorySchema = new Schema({
-    name: { type: String, required: true },
-    type: { type: String, enum: typeEnum },
-    language: { type: String }
-})
+  name: { type: String, required: true },
+  type: { type: String, enum: typeEnum },
+  language: { type: String },
+});
 
 module.exports = mongoose.model('Repository', repositorySchema);
